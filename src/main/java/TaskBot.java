7a64ae11a899b8c2f0be473cc1209eb18ff398ad
h-virdi <harnoorvirdi89@gmail.com>
2025-08-20 14:22:35 +0800

    Mark as Done

--- src/main/java/TaskBot.java
+++ src/main/java/TaskBot.java
@@ -9,7 +9,7 @@ public class TaskBot {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm TaskBot!");
         System.out.println("What can I do for you?");
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
 
         while(true) {
             String command = scanner.nextLine();
@@ -20,15 +20,35 @@ public class TaskBot {
                 System.out.println("____________________________________________________________");
                 break;
             }
+            else if (command.startsWith("mark")) {
+                int taskNum = Integer.parseInt(command.substring(5));
+                Task t = tasks.get(taskNum - 1);
+                t.mark();
+                System.out.println("____________________________________________________________");
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [" + t.getStatusIcon() + "] " + t.getDesc());
+                System.out.println("____________________________________________________________");
+            }
+            else if (command.startsWith("unmark")) {
+                int taskNum = Integer.parseInt(command.substring(7));
+                Task t = tasks.get(taskNum - 1);
+                t.unmark();
+                System.out.println("____________________________________________________________");
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [" + t.getStatusIcon() + "] " + t.getDesc());
+                System.out.println("____________________________________________________________");
+            }
             else if (command.equals("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i));
+                    Task t = tasks.get(i);
+                    System.out.println((i + 1) + ".[" + t.getStatusIcon() + "] " + t.getDesc());
                 }
                 System.out.println("____________________________________________________________");
             }
             else {
-                tasks.add(command);
+                tasks.add(new Task(command));
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + command);
                 System.out.println("____________________________________________________________");
